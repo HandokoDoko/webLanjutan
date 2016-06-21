79c18aa93c345714300c270a1d89ee3eeae7e0e1
alpandi15 <mh2.alpandi@yahoo.co.id>
2016-06-21 14:34:05 +0700

    Add Direction Service Complex

--- public/assets/js/google_maps.js
+++ public/assets/js/google_maps.js
@@ -3,6 +3,7 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
 function initMap() {
+  var markerArray = []; 
   var origin_place_id = null;
   var destination_place_id = null;
   var travel_mode = google.maps.TravelMode.DRIVING;
@@ -19,6 +20,15 @@ function initMap() {
   marker.setMap(map);
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  // Instantiate an info window to hold step text.
+  var stepDisplay = new google.maps.InfoWindow;
+ // Listen to change events from the start and end lists.
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(
+        directionsDisplay, directionsService, markerArray, stepDisplay, map);
+  };
+
   directionsDisplay.setMap(map);
 
   var angkot = document.querySelector("button#angkot");
@@ -83,7 +93,7 @@ map.setZoom(17);
     // If the place has a geometry, store its place ID and route if we have
     // the other place ID
     origin_place_id = place.place_id;
-    route(origin_place_id, "ChIJmbe7_Mo2MTARxpIOmnPC2C0", travel_mode, directionsService, directionsDisplay);
+    route(origin_place_id, "ChIJmbe7_Mo2MTARxpIOmnPC2C0", travel_mode, directionsService, directionsDisplay, stepDisplay, map, markerArray);
     map.setZoom(17);
     marker.setMap(null);
     //route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay);
@@ -105,21 +115,63 @@ map.setZoom(17);
           directionsService, directionsDisplay);
   });*/
 
+
   function route(origin_place_id, destination_place_id, travel_mode,
-                 directionsService, directionsDisplay) {
+                 directionsService, directionsDisplay, stepDisplay, map, markerArray) {
     if (!origin_place_id || !destination_place_id) {
       return;
     }
+
+    // First, remove any existing markers from the map.
+    for (var i = 0; i < markerArray.length; i++) {
+      markerArray[i].setMap(null);
+    }
+
     directionsService.route({
       origin: {'placeId': origin_place_id},
       destination: {'placeId': destination_place_id},
       travelMode: travel_mode
     }, function(response, status) {
       if (status === google.maps.DirectionsStatus.OK) {
+
+        document.getElementById('warnings-panel').innerHTML =
+            '<b>' + response.routes[0].warnings + '</b>';
         directionsDisplay.setDirections(response);
+        showSteps(response, markerArray, stepDisplay, map);
       } else {
         window.alert('Directions request failed due to ' + status);
       }
     });
   }
+
+
+  
+  function showSteps(directionResult, markerArray, stepDisplay, map) {
+    // For each step, place a marker, and add the text to the marker's infowindow.
+    // Also attach the marker to an array so we can keep track of it and remove it
+    // when calculating new routes.
+    var myRoute = directionResult.routes[0].legs[0];
+    for (var i = 0; i < myRoute.steps.length; i++) {
+      var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+      marker.setMap(map);
+      marker.setPosition(myRoute.steps[i].start_location);
+      attachInstructionText(
+          stepDisplay, marker, myRoute.steps[i].instructions, map);
+      //alert(myRoute.steps[i].start_location);
+      var dataArah = [];
+      dataArah[i] = myRoute.steps[i].instructions;
+      //jalur.innerHTML = newElement.childNodes[0].data;
+    }
+    console.log(dataArah);
+  }
+
+  function attachInstructionText(stepDisplay, marker, text, map) {
+    google.maps.event.addListener(marker, 'click', function() {
+    // Open an info window when the marker is clicked on, containing the text
+    // of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
+    alert(text);
+  });
+}
 }
