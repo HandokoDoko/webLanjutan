d89a2c15af751080b156c2b1f92ed0adc24c9a3b
Randy Muhroji <Randy Muhroji>
2016-06-28 13:03:47 +0700

    repair

--- router/admin.js
+++ router/admin.js
@@ -7,13 +7,10 @@ var express = require('express'),
 router = function(app){
 	r.get('/admin', auth, h.index);
 	//r.get('/admin/data_kafe', auth,h.add_kafe);
-<<<<<<< HEAD
 	r.post('/admin/tambah_kafe', h.add_kafe);
 	r.delete('/admin/:id', h.deleteKafe);
-=======
 	r.post('/admin/tambah_kafe', auth, h.add_kafe);
 	
->>>>>>> d224ac97fecfc9af18542dba630e2c7cb34d2c3d
 	app.use(r);
 };
 module.exports = router;
